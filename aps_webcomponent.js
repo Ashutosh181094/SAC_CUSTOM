eea585ec8de6b8d0b18740f3d3dd144fff40bb34
Ashutosh <ashutoshrai181094@gmail.com>
2020-07-12 21:37:57 +0530

    Update aps_webcomponent.js

--- aps_webcomponent.js
+++ aps_webcomponent.js
@@ -13,7 +13,7 @@
 			</fieldset>
 		</form>
                
-                <form id="form">
+                <form id="formColor">
 			<fieldset>
 				<legend>Graph Description Text Color</legend>
 				<table>
@@ -32,6 +32,7 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+			this._shadowRoot.getElementById("formColor").addEventListener("submit", this._submitColor.bind(this));
 		}
 
 		_submit(e) {
@@ -44,6 +45,17 @@
 					}
 			}));
 		}
+		_submitColor(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							widgetText: this.widgetText
+						}
+					}
+			}));
+		}
+
 
 		set widgetText(newText) {
 			this._shadowRoot.getElementById("aps_text").value = newText;
@@ -51,6 +63,14 @@
 
 		get widgetText() {
 			return this._shadowRoot.getElementById("aps_text").value;
+		}
+		
+		set widgetTextColor(newTextColor){
+			this._shadowRoot.getElementById("aps_color").value = newTextColor;
+		}
+		
+		get widgetTextColor(){
+			return this._shadowRoot.getElementById("aps_color").value;
 		}
 	}
 
